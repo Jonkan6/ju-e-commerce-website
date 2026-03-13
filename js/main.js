fa3f44174ad2c15dbd14cabde74d5c5f089fea98
Johanes <lajo25av@student.ju.se>
2026-03-13 14:16:03 +0100

    i added pictures to the products

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ if (productContainer) {
     // Add product card to the page
     productContainer.innerHTML +=
       '<div class="product-card">' +
+      '<img src="' +
+      product.image +
+      '" alt="' +
+      product.name +
+      '">' +
       "<h3>" +
       product.name +
       "</h3>" +
--- js/products.js
+++ js/products.js
@@ -4,20 +4,24 @@ const products = [
     id: 1,
     name: "Gaming PC RTX 4070",
     price: 1499,
+    image: "img/gaming-pc.jpg",
   },
   {
     id: 2,
     name: "Ultrabook Laptop i7",
     price: 999,
+    image: "img/laptop.jpg",
   },
   {
     id: 3,
     name: "Mechanical Keyboard RGB",
     price: 129,
+    image: "img/keyboard.jpg",
   },
   {
     id: 4,
     name: "27-inch 144Hz Monitor",
     price: 299,
+    image: "img/monitor.jpg",
   },
 ];
